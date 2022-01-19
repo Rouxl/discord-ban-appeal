d083480742138269ea04a5a2530127b46fe9c628
James Past <77975528+Rouxl@users.noreply.github.com>
2022-01-19 19:16:05 +0200

    Update Success.js

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -5,7 +5,7 @@ class Success extends Component {
     render() {
         return (
             <Grid item>
-                <h1 style={{textAlign: "center", color: "#6676F3"}}>Sucessfully submitted your appeal!</h1>
+                <h1 style={{textAlign: "center", color: "#6676f3"}}>Sucessfully submitted your appeal!</h1>
                 <h4>We will review your appeal as soon as we can! Abusing this system will result in a permanent ban.</h4>
             </Grid>
         );
