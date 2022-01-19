34c8ecf60c53f6b19d98de4f9bb74c8bc433096e
James Past <77975528+Rouxl@users.noreply.github.com>
2022-01-19 19:15:12 +0200

    update success msg

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -5,8 +5,8 @@ class Success extends Component {
     render() {
         return (
             <Grid item>
-                <h1 style={{textAlign: "center", color: "#00e676"}}>Success! Your ban appeal has been submitted to the mods!</h1>
-                <h4>Please allow some time for them to review your appeal. Abusing this system will result in a perma-ban.</h4>
+                <h1 style={{textAlign: "center", color: "#6676F3"}}>Sucessfully submitted your appeal!</h1>
+                <h4>We will review your appeal as soon as we can! Abusing this system will result in a permanent ban.</h4>
             </Grid>
         );
     }
